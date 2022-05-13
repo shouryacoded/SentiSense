bdd70a18ddb1ab9675b71409fcc1ff6008fd1166
shouryacoded <shourya.online23@gmail.com>
2022-05-13 20:21:02 +0530

    Made the template html and app.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const video = document.getElementById('video');
+
+const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({
+        video :{},
+        audio : false,
+    }).then((stream) => {
+        video.srcObject = stream;
+    });
+};
+
+startVideo();
