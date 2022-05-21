e3b565a488b35725c6a36e0c7f3d2454ceb00ea0
shouryacoded <shourya.online23@gmail.com>
2022-05-21 12:02:52 +0530

    Completed the api

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+//Defining the video feed for the app 
 const video = document.getElementById('video');
 
+// Capturing the video from the browser.
 const startVideo = () => {
     navigator.mediaDevices.getUserMedia({
         video :{},
         audio : false,
     }).then((stream) => {
-        video.srcObject = stream;
+        video.srcObject = stream,
+        err => console.error(err)
     });
-};
+}
 
-startVideo()
+
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/weights'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/weights'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/weights'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/weights')
+]).then(startVideo())
+
+video.addEventListener('playing', () => {
+    const canvas = faceapi.createCanvasFromMedia(video) 
+    document.body.append(canvas)
+    const displaySize = {width : video.width, height : video.height}
+    faceapi.matchDimensions(canvas,displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const adjusted = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, adjusted)
+        faceapi.draw.drawFaceExpressions(canvas, adjusted)
+    }, 100)
+})
