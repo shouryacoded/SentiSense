652a51450223aad210438a96262df670570adf5a
shouryacoded <shourya.online23@gmail.com>
2022-05-28 16:50:31 +0530

    Changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Defining the video feed for the app
 const video = document.getElementById("video");
 const video1 = document.querySelector(".display__video");
-
+let adjusted;
+const emotionMap = new Map([]);
 // Capturing the video from the browser.
 const startVideo = () => {
   navigator.mediaDevices
@@ -14,6 +15,36 @@ const startVideo = () => {
     });
 };
 
+const form_Answer = {
+  name: "",
+  email: "",
+  question1: [
+    "",
+    new Map([
+      ["angry", 0],
+      ["disgusted", 0],
+      ["fearful", 0],
+      ["happy", 0],
+      ["neutral", 0],
+      ["sad", 0],
+      ["surprised", 0],
+    ]),
+    "",
+  ],
+  question2: [
+    "",
+    new Map([
+      ["angry", 0],
+      ["disgusted", 0],
+      ["fearful", 0],
+      ["happy", 0],
+      ["neutral", 0],
+      ["sad", 0],
+      ["surprised", 0],
+    ]),
+    "",
+  ],
+};
 // Loading all the weights from the weights provided by the face-api
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/weights"),
@@ -25,11 +56,9 @@ Promise.all([
 //Overlaying canvas over the video detections.
 video.addEventListener("playing", () => {
   const canvas = faceapi.createCanvasFromMedia(video);
-
   canvas.setAttribute("class", "display__canvas");
   video.insertAdjacentElement("beforebegin", canvas);
   const dimension = video.getBoundingClientRect();
-  console.log(dimension.width);
   const displaySize = { width: dimension.width, height: dimension.height };
   faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
@@ -37,7 +66,7 @@ video.addEventListener("playing", () => {
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions();
-    const adjusted = faceapi.resizeResults(detections, displaySize);
+    adjusted = faceapi.resizeResults(detections, displaySize);
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
@@ -49,17 +78,34 @@ video.addEventListener("playing", () => {
 const form = document.querySelector(".form");
 let positionX = 0;
 let positionY = 0;
+
 form.addEventListener("click", (e) => {
-  console.log(e);
   positionX = e.clientX;
   positionY = e.clientY;
+});
+form.addEventListener("input", (e) => {
+  const question_number = e.target.dataset.question;
+  if (!question_number) return;
+  const expressions = adjusted[0].expressions;
+  const values = Object.values(expressions);
+  const valuesCpy = values.map((value) => value);
+  const keys = Object.keys(expressions);
+  values.sort(function (a, b) {
+    return a - b;
+  });
+  const position = valuesCpy.indexOf(values[6]);
+  const emotion = keys[position];
+  // form_Answer.question1[1].push({ type: emotion });
+  form_Answer[question_number][1].set(
+    emotion,
+    form_Answer[question_number][1].get(emotion) + 1
+  );
 });
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const pointer = document.querySelector(".pointer");
   const result_page = document.querySelector(".container2");
   const show__result = document.querySelector(".container2__show__result");
-  console.log(pointer);
   pointer.style.setProperty("left", `${positionX - 60}px`);
   pointer.style.setProperty("top", `${positionY - 60}px`);
   pointer.style.setProperty("transform", `scaleX(80) scaleY(80)`);
@@ -72,4 +118,30 @@ form.addEventListener("submit", (e) => {
     const container = document.querySelector(".container");
     container.remove();
   }, 2000);
+  const Answers = document.querySelectorAll(".form__input");
+  Answers.forEach((el) => {
+    if (el.id.startsWith("question")) {
+      form_Answer[el.id][0] = el.value;
+    } else {
+      form_Answer[el.id] = el.value;
+    }
+  });
+  calculatefinalEmotion();
 });
+
+function calculatefinalEmotion() {
+  const questions = [form_Answer.question1, form_Answer.question2];
+  questions.forEach((el) => {
+    let max = el[1].get("neutral");
+    console.log(max);
+    el[2] = "neutral";
+    el[1].forEach((value, key) => {
+      if (value > max) {
+        max = value;
+
+        el[2] = key;
+        console.log(el[2], key);
+      }
+    });
+  });
+}
